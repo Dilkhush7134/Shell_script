5b5e12d51ecdb53f88a928a08900d72a86bbd37c
Dilkhush Roy <58264438+Dilkhush7134@users.noreply.github.com>
2024-01-27 22:10:10 +0530

    Update and rename Nodivisibleby2.sh to No_Divisible_by.sh

---
+++ No_Divisible_by.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Number is divisible by 2
+for i in {1..50}; do
+if [ $(expr $i % 2) == 0 ];
+then
+  echo $i
+fi
+done
+
+#======================================Divisible by 3 but not by 5
+for i in {1..10}; do
+if [ $(expr $i % 2) == 0 ] && [ $(expr $i % 3) != 0 ];
+then
+  echo $i
+fi
+done
+
--- Nodivisibleby2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#Number is divisible by 2
-for i in {1..50}; do
-if [ $(expr $i % 2) == 0 ];
-then
-  echo $i
-fi
-done
